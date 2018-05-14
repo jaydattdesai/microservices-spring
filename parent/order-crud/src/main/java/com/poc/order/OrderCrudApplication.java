779b65faec7283e9b19e3a2d9a9b9a827c74f643
Jaydatt <jaydattdesai@gmail.com>
2018-05-14 11:53:58 +0530

    discoverying order crud

--- parent/order-crud/src/main/java/com/poc/order/OrderCrudApplication.java
+++ parent/order-crud/src/main/java/com/poc/order/OrderCrudApplication.java
@@ -2,8 +2,10 @@ package com.poc.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrderCrudApplication {
 
 	public static void main(String[] args) {
